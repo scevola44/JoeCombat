d5dc0a644364cae728c772ea1a1b424b9ad1fb07
Alberto Bandini <scevola@posteo.eu>
2024-05-22 08:44:41 +0200

    started cursing against theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { MaterialModule } from './material/material.module';
 @Component({
     selector: 'app-root', // component name used in markup
     standalone: true, // component is self-contained
     templateUrl: './app.component.html',
-    imports: [RouterOutlet, RouterLink]
+    imports: [RouterOutlet, RouterLink, MaterialModule],
+    styleUrls: ['./app.component.scss']
 })
 // export component
 export class AppComponent implements OnInit{
